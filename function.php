29bb994aa69e8204eb09837b901cb9eccffa4067
MaelysGaido <maelys.gaido@le-campus-numerique.fr>
2020-12-04 15:20:46 +0100

    etape 4 - partie 1

---
+++ function.php
@@ -0,0 +1,36 @@
+<?php
+
+function displayItem1() {
+ echo "jupe Peonie" ; 
+ echo 25;
+ echo '<img src="images/jupe.jpg"> ';
+}
+
+function displayItem2() {
+    echo "robe Lucie" ; 
+    echo 89;
+    echo '<img src="images/robe.jpg"> ';
+}
+
+function displayItem3() {
+    echo "pull Norway" ; 
+    echo 98;
+    echo '<img src="images/pull.jpg"> ';
+}
+//function displayItem2($nom,$prix,$img) {
+ //   $item2 = ["robe Lucie", 85, "images/robe.jpg"];
+  //      foreach ($products as $product) {
+   //      echo $product['nom'],$product['prix'],$product['img'] . "\n";
+   //     }
+    
+//};
+
+//function displayItem3($nom,$prix,$img) {
+ //   $item3 = ["pull Norway", 92, "images/pull.jpg"];
+ //       foreach ($products as $product) {
+  //       echo $product['nom'],$product['prix'],$product['img'] . "\n";
+  //      }
+//};
+
+
+?>
